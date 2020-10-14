cbeb19afe087c39363b71a2ad5d67908876c5808
Jeremias Rößler <sebastian.jeremias.roessler@sap.com>
2020-10-14 16:12:12 +0200

    Add warn and error methods

--- common/persistence/src/main/java/app/coronawarn/server/common/Logger.java
+++ common/persistence/src/main/java/app/coronawarn/server/common/Logger.java
@@ -59,5 +59,112 @@ public class Logger {
     public void info(LogMessages logMessage, Object... arguments) {
     	delegate.info(logMessage.toString(), arguments);
     }
-	
+
+    
+
+    /**
+     * Log a message at the WARN level according to the specified format
+     * and argument.
+     * <p/>
+     * <p>This form avoids superfluous object creation when the logger
+     * is disabled for the WARN level. </p>
+     *
+     * @param format the format string
+     * @param arg    the argument
+     */
+    public void warn(LogMessages logMessage, Object arg){
+    	delegate.warn(logMessage.toString(), arg);
+    }
+
+    /**
+     * Log a message at the WARN level according to the specified format
+     * and arguments.
+     * <p/>
+     * <p>This form avoids superfluous object creation when the logger
+     * is disabled for the WARN level. </p>
+     *
+     * @param format the format string
+     * @param arg1   the first argument
+     * @param arg2   the second argument
+     */
+    public void warn(LogMessages logMessage, Object arg1, Object arg2){
+    	delegate.warn(logMessage.toString(), arg1, arg2);
+    }
+    
+    /**
+     * Log a message at the WARN level according to the specified format
+     * and arguments.
+     * <p/>
+     * <p>This form avoids superfluous string concatenation when the logger
+     * is disabled for the WARN level. However, this variant incurs the hidden
+     * (and relatively small) cost of creating an <code>Object[]</code> before invoking the method,
+     * even if this logger is disabled for WARN. The variants taking
+     * {@link #warn(String, Object) one} and {@link #warn(String, Object, Object) two}
+     * arguments exist solely in order to avoid this hidden cost.</p>
+     *
+     * @param format    the format string
+     * @param arguments a list of 3 or more arguments
+     */
+    public void warn(LogMessages logMessage, Object... arguments){
+    	delegate.warn(logMessage.toString(), arguments);
+    }
+
+
+    /**
+     * Log a message at the ERROR level according to the specified format
+     * and argument.
+     * <p/>
+     * <p>This form avoids superfluous object creation when the logger
+     * is disabled for the ERROR level. </p>
+     *
+     * @param format the format string
+     * @param arg    the argument
+     */
+    public void error(LogMessages logMessage, Object arg){
+    	delegate.error(logMessage.toString(), arg);
+    }
+
+    /**
+     * Log a message at the ERROR level according to the specified format
+     * and arguments.
+     * <p/>
+     * <p>This form avoids superfluous object creation when the logger
+     * is disabled for the ERROR level. </p>
+     *
+     * @param format the format string
+     * @param arg1   the first argument
+     * @param arg2   the second argument
+     */
+    public void error(LogMessages logMessage, Object arg1, Object arg2){
+    	delegate.error(logMessage.toString(), arg1, arg2);
+    }
+
+    /**
+     * Log a message at the ERROR level according to the specified format
+     * and arguments.
+     * <p/>
+     * <p>This form avoids superfluous string concatenation when the logger
+     * is disabled for the ERROR level. However, this variant incurs the hidden
+     * (and relatively small) cost of creating an <code>Object[]</code> before invoking the method,
+     * even if this logger is disabled for ERROR. The variants taking
+     * {@link #error(String, Object) one} and {@link #error(String, Object, Object) two}
+     * arguments exist solely in order to avoid this hidden cost.</p>
+     *
+     * @param format    the format string
+     * @param arguments a list of 3 or more arguments
+     */
+    public void error(LogMessages logMessage, Object... arguments){
+    	delegate.error(logMessage.toString(), arguments);
+    }
+
+    /**
+     * Log an exception (throwable) at the ERROR level with an
+     * accompanying message.
+     *
+     * @param msg the message accompanying the exception
+     * @param t   the exception (throwable) to log
+     */
+    public void error(LogMessages logMessage, Throwable t){
+    	delegate.error(logMessage.toString(), t);
+    }
 }
