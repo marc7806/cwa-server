f31ccd0d6adab876cbc3d83ae365ab2777c323f0
Jeremias Rößler <sebastian.jeremias.roessler@sap.com>
2020-10-14 15:47:21 +0200

    Add remaining log messages to fix compile errors

--- common/persistence/src/main/java/app/coronawarn/server/common/persistence/service/common/LogMessages.java
+++ common/persistence/src/main/java/app/coronawarn/server/common/persistence/service/common/LogMessages.java
@@ -4,6 +4,9 @@ public enum LogMessages {
 
 	KEYS_SELECTED_FOR_UPLOAD("Keys selected for upload: {}"), //
 	KEYS_PICKED_FROM_UPLOAD_TABLE("{} keys picked after read from upload table"), //
+	KEYS_REMAINING_AFTER_FILTERING_BY_CONSENT("{} keys remaining after filtering by consent"), //
+	KEYS_REMAINING_AFTER_FILTERING_BY_VALIDITY("{} keys remaining after filtering by validity"), //
+	KEYS_REMAINING_AFTER_FILTERING_BY_SHARE_POLICY("{} keys remaining after filtering by share policy"), //
 	;
 
 	private final String message;
