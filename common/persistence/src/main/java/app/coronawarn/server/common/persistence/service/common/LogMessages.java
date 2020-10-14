42d1a8339394872f8cec2bdcf8f282649149b99e
Jeremias Rößler <sebastian.jeremias.roessler@sap.com>
2020-10-14 16:23:58 +0200

    Change the remaining log messages to enums

--- common/persistence/src/main/java/app/coronawarn/server/common/persistence/service/common/LogMessages.java
+++ common/persistence/src/main/java/app/coronawarn/server/common/persistence/service/common/LogMessages.java
@@ -7,6 +7,12 @@ public enum LogMessages {
 	KEYS_REMAINING_AFTER_FILTERING_BY_CONSENT("{} keys remaining after filtering by consent"), //
 	KEYS_REMAINING_AFTER_FILTERING_BY_VALIDITY("{} keys remaining after filtering by validity"), //
 	KEYS_REMAINING_AFTER_FILTERING_BY_SHARE_POLICY("{} keys remaining after filtering by share policy"), //
+	MARKET_BATCH_WITH_STATUS("Marked batch with status {}."), //
+	DELETING_BATCH_INFOS_WITH_DATE_OLDER("Deleting {} batch info(s) with a date older than {} day(s) ago."), //
+	NR_RETRIEVED_DISCARDED_DIAGNOSIS_KEYS("Retrieved {} diagnosis key(s). Discarded {} diagnosis key(s) from the result as invalid."), //
+	VALIDATION_FAILED_WITH_VIOLATIONS("Validation failed for diagnosis key from database. Violations: {}"), //
+	DIAGNOSIS_KEYS_CONFLICTED_WITH_DB_ENTRIES("{} out of {} diagnosis keys conflicted with existing database entries and were ignored."), //
+	DELETING_DIAGNOSIS_KEYS_WITH_SUBMISSION_TIMESTAMP_OLDER( "Deleting {} diagnosis key(s) with a submission timestamp older than {} day(s) ago."), //
 	;
 
 	private final String message;
@@ -14,6 +20,7 @@ public enum LogMessages {
 	/**
 	 * @return the log message (default English).
 	 */
+	@Override
 	public String toString() {
 		return message;
 	}
