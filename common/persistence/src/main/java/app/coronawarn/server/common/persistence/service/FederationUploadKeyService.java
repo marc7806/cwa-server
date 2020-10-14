fcd5acc2075a92f209b8b8d92c2911becfa289dd
Jeremias Rößler <sebastian.jeremias.roessler@sap.com>
2020-10-14 15:40:02 +0200

    Add new Logger and LoggerFactory

--- common/persistence/src/main/java/app/coronawarn/server/common/persistence/service/FederationUploadKeyService.java
+++ common/persistence/src/main/java/app/coronawarn/server/common/persistence/service/FederationUploadKeyService.java
@@ -12,14 +12,14 @@ import app.coronawarn.server.common.persistence.domain.FederationUploadKey;
 import app.coronawarn.server.common.persistence.repository.FederationUploadKeyRepository;
 import app.coronawarn.server.common.persistence.service.common.ExpirationPolicy;
 import app.coronawarn.server.common.persistence.service.common.KeySharingPoliciesChecker;
+import app.coronawarn.server.common.persistence.service.common.Logger;
+import app.coronawarn.server.common.persistence.service.common.LoggerFactory;
 import app.coronawarn.server.common.persistence.service.common.ValidDiagnosisKeyFilter;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -65,9 +65,9 @@ public class FederationUploadKeyService {
         .filter(key -> sharingPoliciesChecker.canShareKeyAtTime(key, policy, LocalDateTime.now(UTC)))
         .peek(k -> keysPickedAfterSharePolicy.addAndGet(1))
         .collect(Collectors.toList());
-    logger.info(KEYS_SELECTED_FOR_UPLOAD.toString(), listOfKeys.size());
+    logger.info(KEYS_SELECTED_FOR_UPLOAD, listOfKeys.size());
 
-    logger.info(KEYS_PICKED_FROM_UPLOAD_TABLE.toString(), keysPicked.get());
+    logger.info(KEYS_PICKED_FROM_UPLOAD_TABLE, keysPicked.get());
     logger.info("{} keys remaining after filtering by consent", keysPickedAfterConsent.get());
     logger.info("{} keys remaining after filtering by validity", keysPickedAfterValidity.get());
     logger.info("{} keys remaining after filtering by share policy", keysPickedAfterSharePolicy.get());
---
+++ common/persistence/src/main/java/app/coronawarn/server/common/persistence/service/common/Logger.java
@@ -0,0 +1,61 @@
+package app.coronawarn.server.common.persistence.service.common;
+
+/**
+ * Delegate logger that only accepts and correctly handles {@link LogMessages}.
+ */
+public class Logger {
+
+	private final org.slf4j.Logger delegate;
+
+	public Logger(org.slf4j.Logger logger) {
+		this.delegate = logger;
+	}
+
+    /**
+     * Log a message at the INFO level according to the specified format
+     * and argument.
+     * <p/>
+     * <p>This form avoids superfluous object creation when the logger
+     * is disabled for the INFO level. </p>
+     *
+     * @param format the format string
+     * @param arg    the argument
+     */
+    public void info(LogMessages logMessage, Object arg) {
+    	delegate.info(logMessage.toString(), arg);
+    }
+
+    /**
+     * Log a message at the INFO level according to the specified format
+     * and arguments.
+     * <p/>
+     * <p>This form avoids superfluous object creation when the logger
+     * is disabled for the INFO level. </p>
+     *
+     * @param format the format string
+     * @param arg1   the first argument
+     * @param arg2   the second argument
+     */
+    public void info(LogMessages logMessage, Object arg1, Object arg2) {
+    	delegate.info(logMessage.toString(), arg1, arg2);
+    }
+
+    /**
+     * Log a message at the INFO level according to the specified format
+     * and arguments.
+     * <p/>
+     * <p>This form avoids superfluous string concatenation when the logger
+     * is disabled for the INFO level. However, this variant incurs the hidden
+     * (and relatively small) cost of creating an <code>Object[]</code> before invoking the method,
+     * even if this logger is disabled for INFO. The variants taking
+     * {@link #info(String, Object) one} and {@link #info(String, Object, Object) two}
+     * arguments exist solely in order to avoid this hidden cost.</p>
+     *
+     * @param format    the format string
+     * @param arguments a list of 3 or more arguments
+     */
+    public void info(LogMessages logMessage, Object... arguments) {
+    	delegate.info(logMessage.toString(), arguments);
+    }
+	
+}
