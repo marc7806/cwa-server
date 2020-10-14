18a4b989476afeda4375e18c43e05e1f1889c3f4
Jeremias Rößler <sebastian.jeremias.roessler@sap.com>
2020-10-14 17:40:43 +0200

    Remove unused inputs

--- common/persistence/src/test/java/app/coronawarn/server/common/persistence/domain/config/YamlPropertySourceFactoryTest.java
+++ common/persistence/src/test/java/app/coronawarn/server/common/persistence/domain/config/YamlPropertySourceFactoryTest.java
@@ -1,7 +1,6 @@
 package app.coronawarn.server.common.persistence.domain.config;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
